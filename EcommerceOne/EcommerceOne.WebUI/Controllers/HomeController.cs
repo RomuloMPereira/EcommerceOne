91d81f751187ceb7cddaa62f001aacee631c3bbe
Rômulo Pereira <mp.romulo@gmail.com>
2020-05-26 14:21:21 -0300

    Created Product Filter

--- EcommerceOne/EcommerceOne.WebUI/Controllers/HomeController.cs
+++ EcommerceOne/EcommerceOne.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using EcommerceOne.Core.Contracts;
 using EcommerceOne.Core.Models;
+using EcommerceOne.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,10 +20,24 @@ namespace EcommerceOne.WebUI.Controllers
             this.productCategories = productCategoryContext;
         }
 
-        public ActionResult Index()
+        public ActionResult Index(string category = null)
         {
-            List<Product> products = context.Collection().ToList();
-            return View(products);
+            List<Product> products;
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+
+            if(category == null)
+            {
+                products = context.Collection().ToList();
+            } else
+            {
+                products = context.Collection().Where(p => p.Category == category).ToList();
+            }
+
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+
+            return View(model);
         }
 
         public ActionResult Details(string Id)
