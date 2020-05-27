790b08281c0617cd892c2d81ec67755eb9f514e7
Rômulo Pereira <mp.romulo@gmail.com>
2020-05-27 18:08:58 -0300

    Created BasketUI

--- EcommerceOne/EcommerceOne.WebUI/Controllers/BasketController.cs
+++ EcommerceOne/EcommerceOne.WebUI/Controllers/BasketController.cs
@@ -27,14 +27,14 @@ namespace EcommerceOne.WebUI.Controllers
         {
             basketService.AddToBasket(this.HttpContext, Id);
 
-            return View();
+            return RedirectToAction("Index");
         }
 
         public ActionResult RemoveFromBasket(string Id)
         {
             basketService.RemoveFromBasket(this.HttpContext, Id);
 
-            return View();
+            return RedirectToAction("Index");
         }
 
         public PartialViewResult BasketSummary()
